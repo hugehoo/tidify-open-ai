0bb86c7375983ba0a3bb51054a7fb87df514f0bc
hoo <tbnsok40@gmail.com>
2023-05-28 19:20:21 +0900

    fix: docker buile, push automation

--- tdf-openai.sh
+++ tdf-openai.sh
@@ -1,4 +1,9 @@
-docker build -t my-fastapi-image .
+DOCKER_IMAGE_NAME=tbnsok/my-fastapi-image
+DOCKER_IMAGE_TAG=latest
 
-docker run -d -p 88:88 my-fastapi-image
+docker build -t $DOCKER_IMAGE_NAME/$DOCKER_IMAGE_TAG .
 
+# Push the Docker image to the Docker Hub registry
+docker push $DOCKER_IMAGE_NAME/$DOCKER_IMAGE_TAG
+
+echo "DONE"
